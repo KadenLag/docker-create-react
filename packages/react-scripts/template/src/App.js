a49fdbb94592645c6cf85f1aaf13f2b91ce91a4e
Nick Ritson <nritsonsa@gmail.com>
2018-02-05 13:42:22 +0000

    test image

--- packages/react-scripts/template/src/App.js
+++ packages/react-scripts/template/src/App.js
@@ -11,7 +11,7 @@ class App extends Component {
           <h1 className="App-title">Welcome to React</h1>
         </header>
         <p className="App-intro">
-          I have made a change to the file, pushing to github
+          Changes from Dockercloud and Github?
         </p>
       </div>
     );
